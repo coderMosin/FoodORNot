1fa3919755d9dedd987f2b5eadd077cb9866af40
mosin <17321816410@163.com>
2021-01-04 23:41:24 +0800

    1.存在启动时Property ‘sqlSessionFactory’ or ‘sqlSessionTemplate’ are required问题

--- provider/src/main/java/com/umasou/service/TestApi.java
+++ provider/src/main/java/com/umasou/service/TestApi.java
@@ -1,13 +1,18 @@
 package com.umasou.service;
 
 import com.umasou.ITestApi;
+import com.umasou.dao.INutrition;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @DubboService
 public class TestApi implements ITestApi {
 
+    @Autowired
+    private INutrition iNutrition;
+
     @Override
     public String sayHello() {
-        return "让我试试";
+        return iNutrition.getNutrition().get(0).getName();
     }
 }
